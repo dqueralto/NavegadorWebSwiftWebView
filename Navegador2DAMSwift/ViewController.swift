551da2853a1586f49950bb6d82aaf0c94048ad9b
danielqparra <queraltoies@gmail.com>
2019-01-14 21:15:42 +0100

    Initial Commit

---
+++ Navegador2DAMSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navegador2DAMSwift
+//
+//  Created by Daniel Queraltó Parra on 14/01/2019.
+//  Copyright © 2019 Daniel Queraltó Parra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Navegador2DAMSwiftTests/Navegador2DAMSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Navegador2DAMSwiftTests.swift
+//  Navegador2DAMSwiftTests
+//
+//  Created by Daniel Queraltó Parra on 14/01/2019.
+//  Copyright © 2019 Daniel Queraltó Parra. All rights reserved.
+//
+
+import XCTest
+@testable import Navegador2DAMSwift
+
+class Navegador2DAMSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
