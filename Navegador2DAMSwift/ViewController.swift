691196dd4b004b213afa636d08b023e2efec04f3
danielqparra <queraltoies@gmail.com>
2019-01-15 19:59:50 +0100

    Navegador Swift Web View
    Navegador simple con web View

--- Navegador2DAMSwift/ViewController.swift
+++ Navegador2DAMSwift/ViewController.swift
@@ -8,11 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate, UIWebViewDelegate {
 
+    @IBOutlet weak var barraDeBusqueda: UISearchBar!
+    @IBOutlet weak var webView: UIWebView!
+    
+    @IBAction func atras(_ sender: Any)
+    {
+        if webView.canGoBack
+        {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func recargar(_ sender: Any)
+    {
+        webView.reload()
+    }
+    
+    @IBAction func siguiente(_ sender: Any)
+    {
+        if webView.canGoForward
+        {
+            webView.goForward()
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+    {
+        barraDeBusqueda.resignFirstResponder()
+        
+        if let url = URL(string: barraDeBusqueda.text!)
+        {
+            webView.loadRequest(URLRequest(url: url))
+        }
+        else
+        {
+            print("ERROR")
+        }
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView)
+    {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView)
+    {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+    @IBAction func historial(_ sender: Any)
+    {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        webView.loadRequest(URLRequest(url: URL(string: "http://www.google.com")!))
+        
+        
+        
+
     }
 
 
