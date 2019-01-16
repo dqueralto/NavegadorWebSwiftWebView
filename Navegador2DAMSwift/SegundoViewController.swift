34b9977109db60cb7b341cfed86bd6c329b165ff
danielqparra <queraltoies@gmail.com>
2019-01-16 02:11:22 +0100

    v2

--- Navegador2DAMSwift/SegundoViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SegundoViewController.swift
-//  Navegador2DAMSwift
-//
-//  Created by Daniel Queraltó Parra on 16/01/2019.
-//  Copyright © 2019 Daniel Queraltó Parra. All rights reserved.
-//
-
-import Foundation
