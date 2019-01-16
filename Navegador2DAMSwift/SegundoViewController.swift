6a189410b1fe4152d64bb7ccc8073dc47a7d5a5e
danielqparra <queraltoies@gmail.com>
2019-01-16 02:37:39 +0100

    v2

--- Navegador2DAMSwift/SegundoViewController.swift
+++ Navegador2DAMSwift/SegundoViewController.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 class SegundoViewController: UIViewController {
-
-    @IBOutlet weak var url: UILabel!
     
     
     @IBAction func salir(_ sender: Any)
     {
         self.dismiss(animated: true, completion: nil)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
     
