e4829cee3b3e6f75dd80ea3a36cb8cbc7e644bea
danielqparra <queraltoies@gmail.com>
2019-01-19 18:02:52 +0100

    v2

--- Navegador2DAMSwift/ViewController.swift
+++ Navegador2DAMSwift/ViewController.swift
@@ -70,24 +70,103 @@ class ViewController: UIViewController, UISearchBarDelegate, UIWebViewDelegate {
     
     
     
+    //---------------------------------------------------------------------------------------------------------------
+    //FUNCIONALIDADES
+    //---------------------------------------------------------------------------------------------------------------
     
+    func activarBotones()//HABILITAMOS O DESHABILITAMOS LOS BOTONES DE AVANCE O RETROCESO SEGÚN SE PUEDA
+    {
+        if webKitView.canGoForward
+        {
+            avanzar.isEnabled = true
+        }
+        else
+        {
+            avanzar.isEnabled = false
+        }
+        if webKitView.canGoBack
+        {
+            retroceder.isEnabled = true
+        }
+        else
+        {
+            retroceder.isEnabled = false
+        }
+    }
     
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //performSegue(withIdentifier: "historial", sender: self) //usar para tranferir datos entre ViewControllers
-
-        
         webView.loadRequest(URLRequest(url: URL(string: "http://www.google.com")!))
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    //GESTION DE BASE DE DATOS
+    //---------------------------------------------------------------------------------------------------------------
+    func crearBD()
+    {
+        //INDICAMOS DONDE SE GUARDARA LA BASE DE DATOS Y EL NOMBRE DE ESTAS
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("Historial.sqlite")
+        //INDICAMOS SI DIERA ALGUN FALLO AL CONECTARSE
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("error opening database")
+        }
+        else {//SI PODEMOS CONECTARNOS A LA BASE DE DATOS CREAREMOS LA ESTRUCTURA DE ESTA, SI NO EXISTIERA NO SE HARIA NADA
+            print("base abierta")
+            if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Historial (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT)", nil, nil, nil) != SQLITE_OK {
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("error creating table: \(errmsg)")
+            }
+        }
         
-
-        
-
     }
 
-
+    func insertar()  {
+        //CREAMOS EL PUNTERO DE INSTRUCCIÓN
+        var stmt: OpaquePointer?
+        
+        //CREAMOS NUESTRA SENTENCIA
+        let queryString = "INSERT INTO Historial (url) VALUES ("+"'"+String(barraDeBusqueda.text!)+"')"
+        //PREPARAMOS LA SENTENCIA
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print(queryString)
+            print("error preparing insert: \(errmsg)")
+            return
+        }
+        
+        
+        //EJECUTAMOS LA SENTENCIA PARA INSERTAR LOS VALORES
+        if sqlite3_step(stmt) != SQLITE_DONE {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("fallo al insertar en historial: \(errmsg)")
+            return
+        }
+        
+        //FINALIZAMOS LA SENTENCIA
+        sqlite3_finalize(stmt)
+        //displaying a success message
+        print("Histo saved successfully")
+        
+    }
 }
+//---------------------------------------------------------------------------------------------------------------
+//OBJETOS
+//---------------------------------------------------------------------------------------------------------------
+
+class Histo{
+    
+    var id: Int
+    var url: String?
+    
+    init (id: Int, url: String?)
+    {
+        self.id = id
+        self.url = url
+    }
+    
+}
+
 
